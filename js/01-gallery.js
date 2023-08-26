6f807da690b3995de4dfa5e79f7fb3fc921cc349
Andrij-Panasenko <andrii.panasenko295@gmail.com>
2023-08-26 16:44:16 +0300

    Task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+galleryList.addEventListener("click", onMouseClick);
+
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+  return `<li class="gallery__item">
+    <a href="${original}" class="gallery__link">
+        <img class="gallery__image js-target"
+         src="${preview}" 
+         data-source="${original}" 
+         alt="${description}"
+         />
+    </a>
+</li>`;
+});
+
+galleryList.insertAdjacentHTML("beforeend", markup.join(""))
+
+function onMouseClick(event) {
+    const { target } = event;
+    if (!target.classList.contains("js-target")) {
+        return
+    }
+
+    const originalImg = target.dataset.source;
+    const currentItem = galleryItems.find(({ original }) => original === originalImg);
+
+    const instance = basicLightbox.create(`
+    <div>
+        <img src="${currentItem.original}" alt="${currentItem.description}">
+    </div>
+    `);
+    
+    instance.show();
+
+window.addEventListener("keydown", onEscPress);
+
+    function onEscPress(event) {
+        const ESK_KEY_CODE = event.code === 'Escape'
+        if (ESK_KEY_CODE) {
+            instance.close();
+            window.removeEventListener("keydown", onEscPress)
+        }
+    }
+}
+
+
+
